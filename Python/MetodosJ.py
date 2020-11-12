952fe5febb6834fb544fd8d2eb97adf59ef71e9b
Jonatan Acevedo <jdacevedol@eafit.edu.co>
2020-11-11 23:49:55 -0500

    Metodo TrazlinCubicos

--- Python/MetodosJ.py
+++ Python/MetodosJ.py
@@ -108,9 +108,72 @@ def TrazlinCuadratico(X,Y):
         print(Coef[i,0],"x^2+",Coef[i,1],"x+",Coef[i,2])
         i = i+1
 
+def TrazlinCubicos(X,Y):
+    n = X.size
+    m = 4*(n-1)
+    A = np.zeros((m,m))
+    b = np.zeros((m,1))
+    Coef = np.zeros((n-1,4))
+    i = 0
+    #Condicion de interpolacion
+    while i < X.size-1:
+        
+        A[i+1,4*i:4*i+4]= np.hstack((X[i+1]**3,X[i+1]**2,X[i+1],1)) 
+        b[i+1]=Y[i+1]
+        i = i+1
+
+    A[0,0:4] = np.hstack((X[0]**3,X[0]**2,X[0]**1,1))
+    b[0] = Y[0]
+    #Condicion de continuidad
+    i = 1
+    while i < X.size-1:
+        A[X.size-1+i,4*i-4:4*i+4] = np.hstack((X[i]**3,X[i]**2,X[i],1,-X[i]**3,-X[i]**2,-X[i],-1))
+        b[X.size-1+i] = 0
+        i = i+1
+    #Condicion de suavidad
+    i = 1
+    while i < X.size-1:
+        A[2*n-3+i,4*i-4:4*i+4] = np.hstack((3*X[i]**2,2*X[i],1,0,-3*X[i]**2,-2*X[i],-1,0))
+        b[2*n-3+i] = 0
+        i = i+1
+    
+    #Condicion de concavidad
+    i = 1
+    while i < X.size-1:
+        A[3*n-5+i,4*i-4:4*i+4] = np.hstack((6*X[i],2,0,0,-6*X[i],-2,0,0))
+        b[n+5+i] = 0
+        i = i+1
+    
+    #Condiciones de frontera   
+    A[m-2,0:2]=[6*X[0],2];
+    b[m-2]=0;
+    A[m-1,m-4:m-2]=[6*X[X.size-1],2];
+    b[m-1]=0;
+    
+    Saux = linalg.solve(A,b)
+    print(A)
+    print(b)
+    #Mostrar Coeficientes
+    i = 0
+    j = 0
+    while i < n-1:
+        Coef[i,:] = np.hstack((Saux[j],Saux[j+1],Saux[j+2],Saux[j+3]))
+        i = i+1
+        j = j + 4
+    print("Coeficientes de los trazadores: ")
+    print(Coef)
+
+    #Mostrar trazadores
+    i = 0
+    print("Trazadores: ")
+    while i < X.size-1:
+        print(Coef[i,0],"x^3+",Coef[i,1],"x^2+",Coef[i,2],"x+",Coef[i,3])
+        i = i+1
+
 
 X = np.array([-1, 0, 3, 4])
 Y = np.array([15.5, 3, 8, 1])
 #Lagrange(X,Y)
 #Trazlin(X,Y)
-TrazlinCuadratico(X,Y)
+#TrazlinCuadratico(X,Y)
+TrazlinCubicos(X,Y)
