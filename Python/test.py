da059997194fd8eebbccfe9316034ff219162ff9
Alfredo <ajospinoa@eafit.edu.co>
2020-11-19 00:03:15 -0500

    Pruebas de métodos Jacobi - Newton(diferencias divididas)

--- Python/test.py
+++ Python/test.py
@@ -10,9 +10,19 @@ A = [
 
 b = [1,1,1,1]
 
+x0 = [0,0,0,0]
+
+tol = 0.0000001
+
+Nmax = 100
+
+w = 1.5
+
 X = np.array([-1, 0, 3, 4])
+X2 = [-1, 0, 3, 4]
 
 Y = np.array([15.5, 3, 8, 1])
+Y2 = [15.5, 3, 8, 1]
 
 output = methods.LUSimple(A, b)
 outputToString = methods.outputToString(output)
@@ -27,6 +37,22 @@ output = methods.LUPartialPivot(A, b)
 outputToString = methods.outputToString(output)
 print(outputToString)
 output = methods.LUPartialPivot(A, b)
+outputToString = methods.outputToString(output)
+print(outputToString)
+
+output = methods.jacobiM(A,b,x0,tol,Nmax)
+outputToString = methods.outputToString(output)
+print(outputToString)
+output = methods.gaussSei(A,b,x0,tol,Nmax)
+outputToString = methods.outputToString(output)
+print(outputToString)
+output = methods.sorM(A,b,x0,w,tol,Nmax)
+outputToString = methods.outputToString(output)
+print(outputToString)
+output = methods.vanderMon(X2,Y2)
+outputToString = methods.outputToString(output)
+print(outputToString)
+output = methods.difdivid(X2,Y2)
 outputToString = methods.outputToString(output)
 print(outputToString)
 
